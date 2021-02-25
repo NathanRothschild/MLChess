086c526adb11c9036760dbcd89d59245573f0932
Alexander Lee <alexandersanglee@gmail.com>
2021-02-25 21:24:23 +0900

    tutorial #12, there still is no checkmate alg

--- ChessAI.py
+++ ChessAI.py
@@ -1,4 +1,47 @@
 import random
 
+pieceScore = {"K":0,"Q":10,"R":5,"B":3,"N":3,"p":1}
+CHECKMATE = 1000
+STALEMATE = 0
+
 def findRandomMove(validMoves):
     return random.choice(validMoves)
+
+def findBestMove(gs, validMoves):
+    turnMultiplier = 1 if gs.whiteToMove else -1
+    opponentMinMaxScore = CHECKMATE
+    bestPlayerMove = None
+    random.shuffle(validMoves)
+    for playerMove in validMoves:
+        gs.makeMove(playerMove)
+        opponentsMoves = gs.getValidMoves()
+        opponentMaxScore = -CHECKMATE
+        for opponentsMove in opponentsMoves:
+            gs.makeMove(opponentsMove)
+            if gs.checkmate:
+                score = -turnMultiplier * CHECKMATE
+            elif gs.stalemate:
+                score = STALEMATE
+            else: 
+                score = -turnMultiplier * scoreMaterial(gs.board)
+            if score > opponentMaxScore:
+                opponentMaxScore = score
+            gs.undoMove()    
+        if opponentMaxScore < opponentMinMaxScore:
+            opponentMinMaxScore = opponentMaxScore
+            bestPlayerMove = playerMove
+        gs.undoMove()
+    return bestPlayerMove
+
+
+# Score based on material
+
+def scoreMaterial(board):
+    score = 0
+    for row in board:
+        for square in row:
+            if square[0] == 'w':
+                score += pieceScore[square[1]]
+            elif square[0] == 'b':
+                score -= pieceScore[square[1]] 
+    return score                   
