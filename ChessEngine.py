0d1d7022f02ca816bcbf226c8083504e134e2b8f
Alexander Lee <alexandersanglee@gmail.com>
2021-02-25 22:46:57 +0900

    tut 13

--- ChessEngine.py
+++ ChessEngine.py
@@ -80,6 +80,9 @@ class GameState():
                 else:
                     self.board[move.endRow][move.endCol-2] = self.board[move.endRow][move.endCol+1]
                     self.board[move.endRow][move.endCol+1] = '--'
+
+            self.checkmate = False
+            self.stalemate = False
     
     def updateCastleRights(self, move):
         if move.pieceMoved == 'wK':
