43263556acf041bfcc7539829c674fe82cd00db8
NathanRothschild <ndvr25@gmail.com>
2021-02-25 18:24:39 -0500

    Fixing tiny thing, works great now

--- ChessEngine.py
+++ ChessEngine.py
@@ -305,13 +305,13 @@ class GameState():
                     moves.append(Move((r,c),(r-1,c),self.board))
                     if r == 6 and self.board[r-2][c]=="--":
                         moves.append(Move((r,c),(r-2,c),self.board))
-                elif c-1>=0:    
+                if c-1>=0:    
                     if self.board[r-1][c-1][0]=='b':
                         moves.append(Move((r,c),(r-1,c-1),self.board))
                     elif(r-1, c-1) == self.enpassantPossible:
                         moves.append(Move((r, c), (r-1, c-1), self.board, isEnpassantMove = True))
 
-                elif c+1<=7:
+                if c+1<=7:
                     if self.board[r-1][c+1][0]=='b':
                         moves.append(Move((r,c),(r-1,c+1),self.board))
                     elif(r-1, c+1) == self.enpassantPossible:
@@ -322,12 +322,12 @@ class GameState():
                     moves.append(Move((r,c),(r+1,c),self.board))
                     if r == 1 and self.board[r+2][c]=="--":
                         moves.append(Move((r,c),(r+2,c),self.board))
-                elif c-1>=0:
+                if c-1>=0:
                     if self.board[r+1][c-1][0]=='w':
                         moves.append(Move((r,c),(r+1,c-1),self.board))
                     elif(r+1, c-1) == self.enpassantPossible:
                         moves.append(Move((r, c), (r+1, c-1), self.board, isEnpassantMove = True))
-                elif c+1<= 7:
+                if c+1<= 7:
                     if self.board[r+1][c+1][0]=='w':
                         moves.append(Move((r, c), (r+1, c+1), self.board))
                     elif(r+1, c+1) == self.enpassantPossible:
